d87a9d99117577adcaba75bb6c2b72aa9e729e2d
Ahmed <Umar.Ahmed3@student.tdsb.on.ca>
2016-06-02 13:40:51 -0400

    Added some code for weights and score.

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -17,22 +17,46 @@ class FeedController extends Controller
 				'feed' => []
 			];
 
-			// Selection
-			$atr = str_split($code);
-			$selection = Dog::where('noise_level',$atr[0])
-					->where('activity_level',$atr[1])
-					->where('friend_level',$atr[2])
-					->where('train_level',$atr[3])
-					->where('health_level',$atr[4])
-					->get();
+			// Code must have the correct length
+			if (sizeof($code) == 10) {
 
-			foreach($selection as $dog){
+				$atr = str_split($code);
+				$weight = array(
+					'noise_weight' => $atr[0],
+					'activity_weight' => $atr[2],
+					'friend_weight' => $atr[4],
+					'train_weight' => $atr[6],
+					'health_weight' => $atr[8]
+				);
 
-                $response['feed'][] = [
-                	'id' => $dog->id,
-                    'name' => $dog->name,
-                ];
-            }
+				$score = array(
+					'noise_score' => $atr[1],
+					'activity_score' => $atr[2],
+					'friend_score' => $atr[3],
+					'train_score' => $atr[4],
+					'health_score' => $atr[5]
+				);
+
+				$selection = Dog::where('noise_level',$atr[0])
+						->where('activity_level',$atr[1])
+						->where('friend_level',$atr[2])
+						->where('train_level',$atr[3])
+						->where('health_level',$atr[4])
+						->get();
+
+				foreach($selection as $dog){
+
+	                $response['feed'][] = [
+	                	'id' => $dog->id,
+	                    'name' => $dog->name,
+	                ];
+	            }
+
+			} else {
+
+				throw new Exception("Error Processing Request", 1);
+
+			}
 		} catch (Exception $e) {
 			$statusCode = 400;
 		} finally {
